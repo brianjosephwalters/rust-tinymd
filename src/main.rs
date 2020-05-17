ce59afb22e97ac9f032ae9f6bcd7140e2d4a41d3
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-05-17 15:59:02 -0500

    functions, types, and returns.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+
+fn usage() {
+    println!("tindymd, a markdown compiler written by Brian.");
+    println!("Version {}", get_version());
+}
+
+fn get_version() -> u16 {
+    1000
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
