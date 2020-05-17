34b01a10a887715598d60b86ac6b1a58ced5d5a1
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-05-17 16:20:04 -0500

    Created function setup and help display.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
+fn get_title() -> String {
+    let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+    the_title.push_str(" (v");
+    the_title.push_str(env!("CARGO_PKG_VERSION"));
+    the_title.push_str("), ");
+    the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+
+    the_title
+}
+
+fn parse_markdown_file() {
+
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    println!("Written By: {}", env!("CARGO_PKG_AUTHORS"));
+    println!("Homepage: {}", env!("CARGO_PKG_HOMEPAGE"));
+    println!("Usage: tinymd <somefile>.md");
+}
 
 fn usage() {
-    let mut the_version = env!("CARGO_PKG_VERSION");
-    the_version = "2.0";
-    println!("tindymd, a markdown compiler written by Brian.");
-    println!("Version {}", the_version);
+    print_long_banner();
 }
 
 fn main() {
-    usage();
+  usage();
 }
