9dca34cb227565a7c115bc91836dfc860fe6324c
Brian J. Walters <brian.joseph.walters@gmail.com>
2020-05-17 21:53:08 -0500

    Parse file into tokens.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn get_title() -> String {
     let mut the_title = String::from(env!("CARGO_PKG_NAME"));
     the_title.push_str(" (v");
@@ -8,9 +12,67 @@ fn get_title() -> String {
     the_title
 }
 
-fn parse_markdown_file(_file: &str) {
+fn parse_markdown_file(_filename: &str) {
     print_short_banner();
-    println!("[ INFO ] Trying to parse {}...", _file);
+    println!("[ INFO ] Trying to parse {}...", _filename);
+
+    // Create a path variable from the filename
+    let input_filename = Path::new(_filename);
+    let file = File::open(&input_filename).expect("[ ERROR ] Unable to open the file.");
+
+    let mut _ptag: bool = false;
+    let mut _htag: bool = false;
+
+    let mut tokens: Vec<String> = Vec::new();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let mut output_line = String::new();
+
+        let line_contents = line.unwrap().to_string();
+        let mut first_char: Vec<char> = line_contents.chars().take(1).collect();
+
+        match first_char.pop() {
+            Some('#') => {
+                if _ptag {
+                    _ptag = false;
+                    output_line.push_str("</p>\n");
+                }
+
+                if _htag {
+                    _htag = false;
+                    output_line.push_str("</h1>\n");
+                }
+
+                _htag = true;
+                output_line.push_str("<h1>");
+                output_line.push_str(&line_contents[2..]);
+            },
+            _ => {
+                if !_ptag {
+                    _ptag = true;
+                    output_line.push_str("<p>");
+                }
+                output_line.push_str(&line_contents);
+            }
+        }
+
+        if _ptag {
+            _ptag = false;
+            output_line.push_str("</p>\n");
+        }
+        if _htag {
+            _htag = false;
+            output_line.push_str("</h1>\n");
+        }
+
+        if output_line != "<p></p>\n" {
+            tokens.push(output_line);
+        }
+    }
+
+    for token in &tokens {
+        print!("{}", token);
+    }
 
 }
 
